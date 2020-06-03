0a6d1583fed705974ea7f52ede4ee6897fa52438
edwinb24 <edwin_b24@hotmail.com>
2020-06-02 20:37:03 -0500

    Opps... Forgot the Add task Button

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import classes from "./List.module.css";
 
 export default class List extends Component {
     render() {
         return (
-            <ul className={classes.List}>
-                <li className={classes.Completed}><span>Do Homework</span><button className={classes.Edit}>✏️</button><button className={classes.Remove}>❌</button></li>
-                <li><span>Throw Trash</span><button className={classes.Edit}>✏️</button><button className={classes.Remove}>❌</button></li>
-                <li><span>Pick up kids</span><button className={classes.Edit}>✏️</button><button className={classes.Remove}>❌</button></li>
-            </ul>
-        )
+                <Fragment>
+                    <ul className={classes.List}>
+                        <li className={classes.Completed}><span>Do Homework</span><button className={classes.Edit}>✏️</button><button className={classes.Remove}>❌</button></li>
+                        <li><span>Throw Trash</span><button className={classes.Edit}>✏️</button><button className={classes.Remove}>❌</button></li>
+                        <li><span>Pick up kids</span><button className={classes.Edit}>✏️</button><button className={classes.Remove}>❌</button></li>
+                    </ul>
+                    <div className={classes.addItem}>
+                        <span>+</span>
+                    </div>
+                </Fragment>
+            )
     }
 }
